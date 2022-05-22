334e47164e77f3d57f3cb4537715cba76d3c6869
kris <Kristopher.Kath@gmail.com>
2022-05-22 07:13:34 -0700

    Finished Menu

--- Assets/Scripts/Menu System/MainMenu.cs
+++ Assets/Scripts/Menu System/MainMenu.cs
@@ -17,8 +17,7 @@ namespace MenuAsset
         [Tooltip("Input the desired tutorial scene index. If left at default value then next build index will be loaded.")]
         [SerializeField] private int tutorialIndex = -1;
 
-        [Tooltip("Input the desired credits scene index. If left at default value then next build index will be loaded.")]
-        [SerializeField] private int creditsIndex = -1;
+        [SerializeField] private GameObject creditsDisplay;
 
         [Tooltip("The first button selected to hover on when navigating to options menu.")]
         [SerializeField] private GameObject optionsFirstButton;
@@ -43,20 +42,6 @@ namespace MenuAsset
             if (tutorialIndex != -1)
             {
                 SceneManager.LoadScene(tutorialIndex);
-            }
-            //load next scene in build order
-            else
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
-        }
-
-        public void OpenCredits()
-        {
-            //load given scene value
-            if (creditsIndex != -1)
-            {
-                SceneManager.LoadScene(creditsIndex);
             }
             //load next scene in build order
             else
